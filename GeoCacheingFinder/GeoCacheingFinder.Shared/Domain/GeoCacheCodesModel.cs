117bce336cabef2f80aa95809a369e2a479f0cf5
Daniel-Claus-1314-fhb <clausd@fh-brandenburg.de>
2015-06-01 19:41:40 +0200

    Insert missing models

---
+++ GeoCacheingFinder/GeoCacheingFinder.Shared/Domain/GeoCacheCodesModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Data.Json;
+
+namespace GeoCacheingFinder.Domain
+{
+    class GeoCacheCodesModel
+    {
+        private const string results = "results";
+        private const string seperator = "|";
+
+        public GeoCacheCodesModel() { }
+
+        public GeoCacheCodesModel(List<String> geoCacheCodes)
+        {
+            this.Codes = geoCacheCodes;
+        }
+
+        public GeoCacheCodesModel(String jsonString)
+            : this(JsonObject.Parse(jsonString))
+        { }
+
+        public GeoCacheCodesModel(JsonObject jsonObject)
+        {
+            Codes = new List<String>();
+            foreach (IJsonValue jsonValue in jsonObject.GetNamedArray(results, new JsonArray()))
+            {
+                if (jsonValue.ValueType == JsonValueType.String)
+                {
+                    Codes.Add(jsonValue.GetString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// List of geo cache codes
+        /// </summary>
+        public List<String> Codes;
+
+        public override string ToString()
+        {
+            String strValue = "";
+
+            foreach (String Code in this.Codes)
+            {
+                strValue = strValue + Code + seperator;
+            }
+
+            if (strValue.Length > 0)
+            {
+                strValue = strValue.Substring(0, strValue.Length - 1);
+            }
+            return strValue;
+        }
+    }
+}
